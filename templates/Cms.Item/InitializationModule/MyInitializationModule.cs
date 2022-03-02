dc0ab50cace51a8a0061934b47d3f0fd3642bb2a
Henrik Nystrom <henrik.nystrom@episerver.com>
2022-03-02 16:52:50 +1300

    Add Initialization Module item template

---
+++ templates/Cms.Item/InitializationModule/MyInitializationModule.cs
@@ -0,0 +1,35 @@
+using System;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+#if (configureServices)
+using EPiServer.ServiceLocation;
+using Microsoft.Extensions.DependencyInjection;
+#endif
+
+namespace MyAppNamespace
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Web.InitializationModule))]
+#if (configureServices)
+    public class MyInitializationModule : IConfigurableModule
+    {
+        public void ConfigureContainer(ServiceConfigurationContext context)
+        {
+            // Add services configurations to context.Services
+        }
+
+#else
+    public class MyInitializationModule : IInitializableModule
+    {
+#endif
+        public void Initialize(InitializationEngine context)
+        {
+            // Add initialization logic, this method is called once after CMS has been initialized
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            // Add uninitialization logic
+        }
+    }
+}
