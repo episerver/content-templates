ade15ecb011a86311d1a2c4ba3b0af22fb71849b
Henrik Nystrom <henrik.nystrom@episerver.com>
2022-03-06 09:12:33 +1300

    Add Page Controller template (#23)

---
+++ templates/Cms.Item/MyPageData.cs
@@ -0,0 +1,8 @@
+using EPiServer.Core;
+
+namespace MyAppNamespace
+{
+    // Class used to represent PageData models that can be used
+    // to simplify replacement in templates
+    public class MyPageData : PageData { }
+}
---
+++ templates/Cms.Item/PageController/MyPageController.cs
@@ -0,0 +1,19 @@
+using System;
+using EPiServer.Framework.DataAnnotations;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAppNamespace
+{
+    [TemplateDescriptor(Inherited = true)]
+    public class MyPageController : PageController<MyPageData>
+    {
+        public ActionResult Index(MyPageData currentPage)
+        {
+            // Implementation of action. You can create your own view model class that you pass to the view or
+            // you can pass the page type model directly for simpler templates
+
+            return View(currentPage);
+        }
+    }
+}
