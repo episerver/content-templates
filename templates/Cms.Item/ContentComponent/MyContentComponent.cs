d8d28e464946131a35f6335542e23bf7af4bc9b6
Henrik Nystrom <henrik.nystrom@episerver.com>
2022-03-07 21:03:32 +1300

    Add Content Component template (#24)

---
+++ templates/Cms.Item/ContentComponent/MyContentComponent.cs
@@ -0,0 +1,14 @@
+using System;
+using EPiServer.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyAppNamespace
+{
+    public class MyContentComponent : PartialContentComponent<MyBlockData>
+    {
+        protected override IViewComponentResult InvokeComponent(MyBlockData currentContent)
+        {
+            return View(currentContent);
+        }
+    }
+}
