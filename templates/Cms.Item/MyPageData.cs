5a382648d60e152e8445b6e706db945297c62024
Henrik Nystrom <henrik.nystrom@episerver.com>
2022-03-07 21:03:32 +1300

    Add Content Component template (#24)

--- templates/Cms.Item/MyPageData.cs
+++
@@ -1,8 +0,0 @@
-using EPiServer.Core;
-
-namespace MyAppNamespace
-{
-    // Class used to represent PageData models that can be used
-    // to simplify replacement in templates
-    public class MyPageData : PageData { }
-}
