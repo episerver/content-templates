d08442bfed262bc5db189dc88684e4c84cb6916c
Nguyen Dac Thach <dacthach.nguyen@episerver.com>
2023-02-14 09:40:30 +0700

    introduce AlloyContentAreaItemRenderer and refactor

--- templates/Alloy.Mvc/Business/Initialization/CustomizedRenderingInitialization.cs
+++ templates/Alloy.Mvc/Business/Initialization/CustomizedRenderingInitialization.cs
@@ -20,7 +20,7 @@ public class CustomizedRenderingInitialization : IConfigurableModule
         context.ConfigurationComplete += (o, e) =>
             // Register custom implementations that should be used in favour of the default implementations
             context.Services.AddTransient<IContentRenderer, ErrorHandlingContentRenderer>()
-                .AddTransient<ContentAreaRenderer, AlloyContentAreaRenderer>();
+                .AddSingleton<AlloyContentAreaItemRenderer, AlloyContentAreaItemRenderer>();
     }
 
     public void Initialize(InitializationEngine context) =>
